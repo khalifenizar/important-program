91e0d150fbd6811adfa8c920f511d23ad39355c7
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-10-20 13:43:48 -0400

    Add ImportantThing class

--- important_program.rb
+++ important_program.rb
@@ -1,1 +1,12 @@
-puts "Hi there! What this program does is really important."
+class ImportantThing
+  def initialize(thing)
+    @thing = thing
+  end
+
+  def thing
+    return "You can't have '#{@thing}'. It's too important."
+  end
+end
+
+pizza = ImportantThing.new("pizza")
+puts pizza.thing
